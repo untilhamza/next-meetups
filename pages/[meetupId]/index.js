0ede400be3b9895dcacc4ca269a0f3bb140405ab
Hamza <sanshinehamza@gmail.com>
2022-05-24 15:07:25 +0900

    set fallback to blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
 
   client.close();
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString(),
